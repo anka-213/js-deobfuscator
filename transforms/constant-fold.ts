ca9bb24cf7a351dc9fe991f344d1e0b441e2e957
Andreas Källberg <anka.213@gmail.com>
2021-06-04 18:03:46 +0800

    More steps

--- transforms/constant-fold.ts
+++ transforms/constant-fold.ts
@@ -1,16 +1,17 @@
 // transforms/implicit-icons-to-explicit-imports.ts
-import {
+import core, {
   ASTNode,
   ASTPath,
   Identifier,
   JSCodeshift,
   MemberExpression,
+  Pattern,
   Transform,
   VariableDeclaration,
   VariableDeclarator,
 } from "jscodeshift";
 import { isValidIdentifier } from "@babel/types";
-import { Collection } from "jscodeshift";
+// import { Collection } from "jscodeshift";
 import { Type } from "ast-types/lib/types";
 
 const transform: Transform = (file, api, options) => {
@@ -56,6 +57,86 @@ const transform: Transform = (file, api, options) => {
       return ans;
     });
   console.log(`Transformed ${dotExprs.length} member-expressions`);
+
+  // Rename variables
+  {
+    let varNr = 1;
+    const varDecls = root.findVariableDeclarators();
+    varDecls.forEach((pth, i) => {
+      // const vd: Collection<VariableDeclarator> = j(pth);
+      const vd = varDecls.at(i);
+      vd.renameTo("v" + varNr);
+      varNr++;
+    });
+  }
+
+  // Rename functions
+  {
+    let funNr = 1;
+    const funDecls = root.find(j.FunctionDeclaration);
+    funDecls.forEach((vd) => {
+      // const vd: Collection<VariableDeclarator> = j(pth);
+      const oldName = vd.value.id.name;
+      const newName = "f" + funNr++;
+      const rootScope = vd.parentPath.scope;
+      const jScope = j(vd.parentPath).closestScope();
+      // const vd = funDecls.at(i);
+      // pth.value.id.name
+      // vd.renameTo("f" + varNr);
+      jScope
+        .find(j.Identifier, { name: oldName })
+        .filter(isVariable(j))
+        .forEach((path) => {
+          // identifier must refer to declared variable
+          let scope = path.scope;
+          while (scope && scope !== rootScope) {
+            if (scope.declares(oldName)) return;
+            scope = scope.parent;
+          }
+          if (!scope) return; // The variable must be declared
+          path.get("name").replace(newName);
+        });
+      // varNr++;
+    });
+  }
+
+  // Rename arguments
+  const funDeclsA = root.find(j.Function);
+  let anon = 1;
+  funDeclsA.forEach((fd, i, pths) => {
+    // const vd: Collection<VariableDeclarator> = j(pth);
+    let argNr = 1;
+    const jvd = funDeclsA.at(i);
+    const fname = fd.value.id?.name ?? "anon" + anon++;
+    const args = jvd
+      .map((x) => x.get("params").map((x) => x, null))
+      .filter(checkPath(j.Identifier));
+    console.log(args);
+    args.forEach((vd) => {
+      const oldName = vd.value.name;
+      const newName = fname + "_arg" + argNr++;
+      const rootScope = vd.scope;
+      const jScope = j(vd).closestScope();
+      // const vd = funDecls.at(i);
+      // pth.value.id.name
+      // vd.renameTo("f" + varNr);
+      jScope
+        .find(j.Identifier, { name: oldName })
+        .filter(isVariable(j))
+        .forEach((path) => {
+          // identifier must refer to declared variable
+          let scope = path.scope;
+          while (scope && scope !== rootScope) {
+            if (scope.declares(oldName)) return;
+            scope = scope.parent;
+          }
+          if (!scope) return; // The variable must be declared
+          path.get("name").replace(newName);
+        });
+    });
+  });
+
+  // Remove aliases like "var foo = bar;" and replace foo with bar
   const varisvar = root
     .find(j.VariableDeclarator, {
       type: "VariableDeclarator",
@@ -64,7 +145,11 @@ const transform: Transform = (file, api, options) => {
     })
     .forEach((vd) => {
       // No pattern matching declarations
-      if (vd.value.id.type != "Identifier" || vd.value.init.type != "Identifier") return null;
+      if (
+        vd.value.id.type != "Identifier" ||
+        vd.value.init.type != "Identifier"
+      )
+        return null;
       const oldName = vd.value.id.name;
       const newName = vd.value.init.name;
       const rootScope = vd.scope;
@@ -80,53 +165,83 @@ const transform: Transform = (file, api, options) => {
       jScope
         .find(j.Identifier, { name: oldName })
         .filter(isVariable(j))
-        .forEach((path) => { // identifier must refer to declared variable
+        .forEach((path) => {
+          // identifier must refer to declared variable
           let scope = path.scope;
           while (scope && scope !== rootScope) {
             if (scope.declares(oldName)) return;
             scope = scope.parent;
           }
           if (!scope) return; // The variable must be declared
-          path.get('name').replace(newName)
+          path.get("name").replace(newName);
         });
+      vd.prune();
     });
+
+  // Simplify function return
   // const funs = root.find(j.FunctionDeclaration,{expression: false, body: {type: "BlockStatement"}})
   // let funBod = root.find(j.FunctionDeclaration).paths()[0].value.body.body
   let funBod = root.find(j.FunctionDeclaration).forEach((fun) => {
     let stmts = fun.get("body", "body");
-    let last = stmts.get(stmts.value.length - 2);
+    let second_last = stmts.get(stmts.value.length - 2);
     // if (!j.VariableDeclaration.check(last.value)) return null
-    if (!checkPath(last, j.VariableDeclaration)) return null;
-    last.value;
-    if (last.value.type === "VariableDeclaration") {
-      let last1: ASTPath<VariableDeclaration> =
-        last as ASTPath<VariableDeclaration>;
-      const varDecl = last1.value.declarations[0];
-      if (varDecl.type !== "VariableDeclarator") return;
-      if (varDecl.id.type !== "Identifier") return;
-      const oldName = varDecl.id.name;
-      const scope = j(last1).closestScope();
-      // scope.find(j.Identifier, {name: last1.value.id.name})
+    if (!checkPath(j.VariableDeclaration)(second_last)) return null;
 
-      let foo = last1.get("declarations", 0);
-      last.value.declarations;
-      // foo.
-    }
-    stmts.value.reduceRight((prev, curr, idx, arr) => {
-      if (
-        prev.type === "ReturnStatement" &&
-        curr.type === "VariableDeclaration" &&
-        curr.declarations.length === 1
-      ) {
-        let [dec] = curr.declarations;
-        if (dec.type == "VariableDeclarator" && dec.id.type === "Identifier") {
-          // dec.id.scope
-        }
-      }
-      // j.match(curr, {type: "VariableDeclaration"})
-      // j.match(curr,j.variableDeclaration("var",[j.variableDeclarator(j.identifier("foo"))]))
-      return curr;
+    second_last.value;
+    // We only support a single declarator for now
+    if (second_last.value.declarations.length != 1) return null
+    // const varDecl = second_last.value.declarations[0];
+    const varDecl = second_last.get("declarations", 0)
+    if (!checkPath(j.VariableDeclarator)(varDecl)) return;
+    if (varDecl.value.id.type !== "Identifier") return;
+    const oldName = varDecl.value.id.name;
+    const scope = j(second_last).closestScope();
+    const vd = varDecl
+    // scope.find(j.Identifier, {name: last1.value.id.name})
+
+    const newValue = vd.value.init;
+    const rootScope = vd.scope;
+    const jScope = j(vd).closestScope();
+    // vd.scope.bindings
+
+    // We don't replace if the value is changed at any point
+    const changes = jScope.find(j.AssignmentExpression, {
+      left: { type: "Identifier", name: oldName },
     });
+    if (changes.length > 0) return null;
+
+    jScope
+      .find(j.Identifier, { name: oldName })
+      .filter(isVariable(j))
+      .forEach((path) => {
+        // identifier must refer to declared variable
+        let scope = path.scope;
+        while (scope && scope !== rootScope) {
+          if (scope.declares(oldName)) return;
+          scope = scope.parent;
+        }
+        if (!scope) return; // The variable must be declared
+        path.replace(newValue);
+      });
+
+    // let foo = second_last.get("declarations", 0);
+    // second_last.value.declarations;
+    // // foo.
+    // stmts.value.reduceRight((prev, curr, idx, arr) => {
+    //   if (
+    //     prev.type === "ReturnStatement" &&
+    //     curr.type === "VariableDeclaration" &&
+    //     curr.declarations.length === 1
+    //   ) {
+    //     let [dec] = curr.declarations;
+    //     if (dec.type == "VariableDeclarator" && dec.id.type === "Identifier") {
+    //       // dec.id.scope
+    //     }
+    //   }
+    //   // j.match(curr, {type: "VariableDeclaration"})
+    //   // j.match(curr,j.variableDeclaration("var",[j.variableDeclarator(j.identifier("foo"))]))
+    //   return curr;
+    // });
     // stmts.replace([]);
   });
 
@@ -136,6 +251,7 @@ const transform: Transform = (file, api, options) => {
 };
 
 const isVariable = (j: JSCodeshift) => (path: ASTPath<Identifier>) => {
+  const j = core
   // ignore non-variables
   const parent: unknown = path.parent.node;
 
@@ -189,11 +305,11 @@ const isVariable = (j: JSCodeshift) => (path: ASTPath<Identifier>) => {
   return true;
 };
 
-function checkPath<T extends ASTNode>(
-  pth: ASTPath,
-  t: Type<T>
-): pth is ASTPath<T> {
-  return t.check(pth.value);
-}
+const checkPath =
+  <T extends ASTNode>(t: Type<T>) =>
+  (pth: ASTPath<any>): pth is ASTPath<T> => {
+    return t.check(pth.value);
+  };
+// (pth: ASTPath): pth is ASTPath<T> => {
 
 export default transform;
