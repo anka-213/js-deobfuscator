5496f96da041170634aa313eac056f240f753fc6
Andreas Källberg <anka.213@gmail.com>
2021-06-05 13:10:54 +0800

    Renumber simplified output

--- transforms/__testfixtures__/breakthecode.simplified.output.js
+++ transforms/__testfixtures__/breakthecode.simplified.output.js
@@ -1,7 +1,7 @@
 'use strict';
 // Version with the function for rotating the array already (manually) executed
 var v1 = ["680575VSjskW", "hidden", "#app", "input", "add", "data", "application/json", "1138771DnkNzm", "querySelector", "2XDTygo", "1079yilIzd", "368168VvQIgG", "14081gJOvPZ", "getAttribute", "208866QssNBc", "/puzzle-new", "cooldown", "response", "remove", "modal-open", "status", "flags", "1512231DKtOVj", "then", "loading", "error", "disableSubmit", "3171IGRaWO", ".answer-popup-success", "127LGCEKU"]
-var v6 = new Vue({
+var v2 = new Vue({
     'el': v1[2],
     'data': {
         'input': '',
@@ -18,28 +18,28 @@ var v6 = new Vue({
     },
     'methods': {
         'submit': function submit() {
-            var v9 = this;
+            var v3 = this;
             !this[v1[26]] && this[v1[3]] !== '' && (this[v1[21]][v1[25]] = false,
             this[v1[21]].loading = true,
             setTimeout(function() {
                 axios.post(v1[15], {
-                    'answer': v9[v1[3]]
+                    'answer': v3[v1[3]]
                 }, {
                     'headers': {
                         'X-CSRF-TOKEN': document[v1[8]]('meta[name=\x22csrf-token\x22]')[v1[13]]('content'),
                         'Content-Type': v1[6]
                     }
-                })[v1[23]](function(anon3_arg1) {
-                    anon3_arg1.data && (v9.flags[v1[24]] = false,
+                })[v1[23]](function(anon2_arg1) {
+                    anon2_arg1.data && (v3.flags[v1[24]] = false,
                     document[v1[8]]('html,\x20body').classList[v1[4]](v1[19]),
-                    document[v1[8]]('.answer-popup__div').innerHTML = anon3_arg1[v1[5]].content,
+                    document[v1[8]]('.answer-popup__div').innerHTML = anon2_arg1[v1[5]].content,
                     document[v1[8]](v1[28]).classList[v1[18]](v1[1]));
-                })['catch'](function(anon4_arg1) {
-                    anon4_arg1[v1[17]] && ((anon4_arg1.response[v1[20]] == 400 || anon4_arg1[v1[17]][v1[20]] == 422 || anon4_arg1[v1[17]][v1[20]] == 429) && (v9[v1[21]][v1[24]] = false,
-                    v9.flags[v1[25]] = true,
-                    v9[v1[21]][v1[16]] = true,
+                })['catch'](function(anon3_arg1) {
+                    anon3_arg1[v1[17]] && ((anon3_arg1.response[v1[20]] == 400 || anon3_arg1[v1[17]][v1[20]] == 422 || anon3_arg1[v1[17]][v1[20]] == 429) && (v3[v1[21]][v1[24]] = false,
+                    v3.flags[v1[25]] = true,
+                    v3[v1[21]][v1[16]] = true,
                     setTimeout(function() {
-                        v9[v1[21]][v1[16]] = false;
+                        v3[v1[21]][v1[16]] = false;
                     }, 5000)));
                 });
             }, 1500));
