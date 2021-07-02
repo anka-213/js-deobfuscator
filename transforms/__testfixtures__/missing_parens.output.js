3bfaa8ce5c1c4a4f00d3f0d85b32cbb7b1dd102c
Andreas Källberg <anka.213@gmail.com>
2021-07-02 23:15:36 +0800

    Add test case

---
+++ transforms/__testfixtures__/missing_parens.output.js
@@ -0,0 +1,40 @@
+null != (_0x4fda33 = _0x432a5f._w).GG;
+
+_0x47f3d0 =
+  (anon1_arg1) =>
+  (
+    anon2_arg1,
+    anon2_arg2,
+    anon2_arg3,
+    anon2_arg4,
+    anon2_arg5,
+    anon2_arg6,
+    anon2_arg7,
+    anon2_arg8,
+    anon2_arg9,
+    anon2_arg10,
+    anon2_arg11,
+    anon2_arg12,
+    anon2_arg13
+  ) =>
+    ((anon2_arg2 = _0x1c396f(anon2_arg3)
+      ? ""
+      : _0x1c396f(anon2_arg4)
+      ? ">=" + anon2_arg3 + a0_0x5dad("0x32b1") + (anon1_arg1 ? "-0" : "")
+      : _0x1c396f(anon2_arg5)
+      ? ">=" + anon2_arg3 + "." + anon2_arg4 + ".0" + (anon1_arg1 ? "-0" : "")
+      : anon2_arg6
+      ? ">=" + anon2_arg2
+      : ">=" + anon2_arg2 + (anon1_arg1 ? "-0" : "")) +
+      "\x20" +
+      (anon2_arg8 = _0x1c396f(anon2_arg9)
+        ? ""
+        : _0x1c396f(anon2_arg10)
+        ? "<" + (+anon2_arg9 + 1) + a0_0x5dad("0x323a")
+        : _0x1c396f(anon2_arg11)
+        ? "<" + anon2_arg9 + "." + (+anon2_arg10 + 1) + a0_0x5dad("0x7d")
+        : anon2_arg12
+        ? "<=" + anon2_arg9 + "." + anon2_arg10 + "." + anon2_arg11 + "-" + anon2_arg12
+        : anon1_arg1
+        ? "<" + anon2_arg9 + "." + anon2_arg10 + "." + (+anon2_arg11 + 1) + "-0"
+        : "<=" + anon2_arg8))[a0_0x5dad("0x3b8d")]();
--- transforms/__tests__/constant-fold.ts
+++ transforms/__tests__/constant-fold.ts
@@ -1,8 +1,14 @@
 // transforms/__tests__/constant-fold.ts
 import { defineTest } from "jscodeshift/dist/testUtils";
 
-describe("implicit-icons-to-explicit-imports", () => {
+describe("deobfuscate-array-code", () => {
   defineTest(__dirname, "constant-fold", null, `breakthecode`, {
+    parser: "babel",
+  });
+  defineTest(__dirname, "constant-fold", null, `breakthecode.simplified`, {
+    parser: "babel",
+  });
+  defineTest(__dirname, "constant-fold", null, `missing_parens`, {
     parser: "babel",
   });
 });
